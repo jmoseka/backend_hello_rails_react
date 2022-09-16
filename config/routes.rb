ebc7681c5a62a9760338d7235ba4a3540c880568
jmoseka <jamylam3@gmail.com>
2022-09-16 21:19:29 +0300

    Create table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'greetings/index'
+      get 'greetings/show'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
